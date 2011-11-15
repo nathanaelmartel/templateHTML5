b573cbea52a49d5802ed3d26e466f7e5ef91daf3
Nathanaël Martel <nathanael@fam-martel.eu>
2011-11-15 15:29:31 +0100

    Update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-
   $(document).ready(function () {
 
-	  $("a[href^=http://]").not("a[href^=http://www.yourdomain]").attr("target", "blank");  
-	  $("a[href^=http://]").not("a[href^=http://www.yourdomain]").attr("rel", "external");
-  
   });
   
